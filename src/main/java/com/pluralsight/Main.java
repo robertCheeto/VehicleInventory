71d30f50785ec5b04551635efa8ed1808fe100cd
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 15:59:24 -0400

    * trying to replicate pre made array topher made

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Vehicle[] usedCarArray = new Vehicle[20];
         //usedCarArray[0] = new Vehicle[]{101121L, "Ford Explorer", "white", 50000, 17590f};
-        usedCarArray.add(101121L, "Ford Explorer", "white", 50000, 17590f);
+        //usedCarArray.add(101121L, "Ford Explorer", "white", 50000, 17590f);
+
+        Vehicle[] carList = {
+                (101121L, "Ford Explorer", "white", 50000, 17590),
+
+        };
         System.out.println(usedCarArray);
         // need to determine when I put the 6 cars into the array
         // I think that needs to be initialized in the Vehicle file
