92a9aa8a555f3adbe9e7625da2b550078abdd83e
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 14:13:39 -0400

    * created Vehicle class and created Vehicle constructor * created getters/setters for each of the car's properties

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,57 @@
+package com.pluralsight;
+
+public class Vehicle {
+    private long vehicleID;
+    private String makeModel;
+    private String color;
+    private int odometerReading;
+    private float price;
+
+    public Vehicle() {
+        this.vehicleID = 0;
+        this.makeModel = "";
+        this.color = "";
+        this.odometerReading = 0;
+        this.price = 0;
+    } // parameterless Vehicle Constructor
+
+    public long getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(long vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+
+    public void setOdometerReading(int odometerReading) {
+        this.odometerReading = odometerReading;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
