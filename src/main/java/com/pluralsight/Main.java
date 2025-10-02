f123963750255d991e1376312b4a78c5f64aa067
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 16:07:47 -0400

    * started creating default array with 6 cars preloaded * need to add String exception in Vehicle Class to display the array instead of mem location

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.util.Vector;
+
 public class Main {
     public static void main(String[] args) {
         Vehicle[] usedCarArray = new Vehicle[20];
@@ -7,10 +9,11 @@ public class Main {
         //usedCarArray.add(101121L, "Ford Explorer", "white", 50000, 17590f);
 
         Vehicle[] carList = {
-                (101121L, "Ford Explorer", "white", 50000, 17590),
+                new Vehicle(101121, "Ford Explorer", "white", 50000, 17590),
+                //new Vehicle()
 
         };
-        System.out.println(usedCarArray);
+        System.out.println(carList); // need to add String exception thing to get strings as opposed to mem values
         // need to determine when I put the 6 cars into the array
         // I think that needs to be initialized in the Vehicle file
 
