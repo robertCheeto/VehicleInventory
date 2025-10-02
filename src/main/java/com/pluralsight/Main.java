73c8fc2a2d7d22c07b52d10ca2a510955cb65734
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 15:02:26 -0400

    * figuring out how to add one example car to the array given a set of information

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,6 +3,9 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
         Vehicle[] usedCarArray = new Vehicle[20];
+        //usedCarArray[0] = new Vehicle[]{101121L, "Ford Explorer", "white", 50000, 17590f};
+        usedCarArray.add(101121L, "Ford Explorer", "white", 50000, 17590f);
+        System.out.println(usedCarArray);
         // need to determine when I put the 6 cars into the array
         // I think that needs to be initialized in the Vehicle file
 
