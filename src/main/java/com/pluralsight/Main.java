7a2eb23909125931c305ad5d46de7ff3cd83b4e7
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 14:41:59 -0400

    * created an emptyArray with 20 spaces

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,9 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        // first commit
+        Vehicle[] usedCarArray = new Vehicle[20];
+        // need to determine when I put the 6 cars into the array
+        // I think that needs to be initialized in the Vehicle file
+
     }
 }
